5a39ea3bcda360b3248f73b24f0be5149f34b680
bpatriciaella <bpatriciaella@yahoo.com>
2015-01-09 17:24:38 +0100

    Allows to generate hash of text and removed some silly bug

--- GenerateMd5Hash/Program.cs
+++ GenerateMd5Hash/Program.cs
@@ -11,32 +11,54 @@ namespace GenerateMd5Hash
         [STAThread]
         static void Main(string[] args)
         {
-            #region Basic file- check
+            var bIsFile = false;
+            var text = String.Empty;
+
+            #region Basic checks...
 
             if (args.Length <= 0)
             {
-                Console.WriteLine("You really need to specify a file!");
+                Console.WriteLine("You really need to specify a file/ string!");
+                PrintHelp();
                 Environment.Exit(401);
+            }
+
+            if (args[0].StartsWith("--?"))
+            {
+                PrintHelp();
+                Environment.Exit(40);
             }
 
             if (!File.Exists(args[0]))
             {
-                Console.WriteLine("File not found!");
-                Environment.Exit(404);
+                bIsFile = false;
+                text = args[0].StartsWith("@") ? args[0].Substring(1) : args[0];
+            }
+
+            else
+            {
+                bIsFile = true;
+                
             }
 
             #endregion
 
 
-            byte[] bHashBuffer;
+            byte[] bHashBuffer = new byte[0];
 
             using (var md5 = MD5.Create())
             {
-
-                using (var fs = File.OpenRead(args[0]))
+                if (bIsFile)
                 {
-                    bHashBuffer = new byte[fs.Length];
-                    bHashBuffer = md5.ComputeHash(fs);
+                    using (var fs = File.OpenRead(args[0]))
+                    {
+                        bHashBuffer = md5.ComputeHash(fs);
+                    }
+                }
+
+                else
+                {
+                    bHashBuffer = md5.ComputeHash(Encoding.UTF8.GetBytes(text));
                 }
             }
 
@@ -52,8 +74,41 @@ namespace GenerateMd5Hash
             if (key.Key == ConsoleKey.Y)
             {
                 Clipboard.SetText(strComputedHash.ToString());
-                Console.WriteLine("Copied to Clipboard!");
+                Console.WriteLine("\nCopied to Clipboard!");
                 Console.WriteLine("");
+            }
+        }
+
+        static void PrintHelp()
+        {
+            const int iPosition = 30;
+
+            FillConsoleLineWithChar("-");
+            Console.Write("| --help");
+            Console.SetCursorPosition(iPosition, Console.CursorTop);
+            Console.Write("Prints helptext");
+            Console.SetCursorPosition(Console.BufferWidth - 1, Console.CursorTop);
+            Console.Write("|");
+            FillConsoleLineWithChar("-");
+
+            Console.Write("| Generate md5sum of File:");
+            Console.SetCursorPosition(iPosition, Console.CursorTop);
+            Console.Write("[PATH_TO_FILE]");
+            Console.SetCursorPosition(Console.BufferWidth - 1, Console.CursorTop);
+            Console.Write("|");
+            Console.Write("| Generate md5sum of text:");
+            Console.SetCursorPosition(iPosition, Console.CursorTop);
+            Console.Write("@[TEXT]");
+            Console.SetCursorPosition(Console.BufferWidth - 1, Console.CursorTop);
+            Console.Write("|");
+            FillConsoleLineWithChar("-");
+        }
+
+        static void FillConsoleLineWithChar(string character)
+        {
+            for (var i = 0; i < Console.BufferWidth; i++)
+            {
+                Console.Write(character);
             }
         }
     }
